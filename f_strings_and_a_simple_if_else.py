c7ab90bbefd74a8a4c372b161942f22ee2389962
Lotanna <pythonchal@gmail.com>
2019-10-01 02:08:43 -0700

    added f-strings and a simple if else

--- f_strings_and_a_simple_if_else.py
+++ f_strings_and_a_simple_if_else.py
@@ -6,3 +6,10 @@ def allowed_driving(name : str, age: int) -> None:
        checking the passed in age against the MIN_DRIVING_AGE constant"""
     allowed_to_drive = f"{name} is allowed to drive" if age >= MIN_DRIVING_AGE else f"{name} is not allowed to drive"
     print(allowed_to_drive)
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    is_allowed_or_not = "is allowed" if age >= MIN_DRIVING_AGE else "is not allowed" # Optimized space completxity
+    print(f"{name} {is_allowed_or_not} to drive")
