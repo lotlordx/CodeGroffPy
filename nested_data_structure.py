3511de504af03ad7f39aa45eee4ee0c594da7c98
Lotanna <pythonchal@gmail.com>
2019-10-09 00:33:56 -0700

    nested data sturcture

--- nested_data_structure.py
+++ nested_data_structure.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 cars = {
     'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
     'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
@@ -18,15 +20,19 @@ def get_first_model_each_manufacturer(cars=cars):
     return [x[0] for x in cars.values()]
 
 
-
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    return [match for car_values in sorted(cars.values()) for match in car_values if grep.lower() in match.lower()]
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    pass
+    return {key: sorted(value) for key, value in cars.items()}
+
+
+
+models = list(chain.from_iterable(cars.values()))
+print(models)
